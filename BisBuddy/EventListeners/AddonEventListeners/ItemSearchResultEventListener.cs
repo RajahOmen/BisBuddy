3bd6600f472bdbe9657abd701606c29858a58641
RajahOmen <noah@reinrag.net>
2025-04-10 16:02:55 -0400

    highlight listings in marketboard results based on qty needed

--- BisBuddy/EventListeners/AddonEventListeners/ItemSearchResultEventListener.cs
+++ BisBuddy/EventListeners/AddonEventListeners/ItemSearchResultEventListener.cs
@@ -2,12 +2,16 @@ using BisBuddy.Gear;
 using BisBuddy.Util;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using Dalamud.Utility;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Info;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.Classes;
 using KamiToolKit.Nodes;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using static FFXIVClientStructs.FFXIV.Component.GUI.AtkComponentList;
 
 namespace BisBuddy.EventListeners.AddonEventListeners
 {
@@ -28,11 +32,13 @@ namespace BisBuddy.EventListeners.AddonEventListeners
         public static readonly uint AddonItemScrollbarNodeId = 5;
         // id of the scroll button on the bar
         public static readonly uint AddonItemScrollButtonNodeId = 2;
+        // id of the text node containing the quantity of the item being sold in the result
+        public static readonly uint AddonItemQuantityNodeId = 6;
 
 
         // if nq or hq is needed for the selected item
-        private bool nqNeeded = false;
-        private bool hqNeeded = false;
+        private int nqNeeded = 0;
+        private int hqNeeded = 0;
         // the previous Y value of the scrollbar
         private float previousScrollbarY = -1.0f;
 
@@ -72,15 +78,20 @@ namespace BisBuddy.EventListeners.AddonEventListeners
         {
             try
             {
-                if (!hqNeeded && !nqNeeded)
+                if (hqNeeded == 0 && nqNeeded == 0)
                 { // no variant needed for this selection, unmark and quit
                     unmarkAllNodes();
                     return;
                 };
                 var addon = (AddonItemSearchResult*)Services.GameGui.GetAddonByName(AddonName);
-                if (addon == null || !addon->IsVisible) return; // addon not visible/rendered
 
-                if (addon->Results == null || addon->Results->ListLength == 0) return; // no items for sale
+                // addon not visible/rendered
+                if (addon == null || !addon->IsVisible)
+                    return;
+
+                // no items for sale
+                if (addon->Results == null || addon->Results->ListLength == 0)
+                    return;
 
                 var listingList = addon->Results;
 
@@ -98,72 +109,73 @@ namespace BisBuddy.EventListeners.AddonEventListeners
                 else
                 {
                     // scrollbar in same position, nodes haven't changed
-                    if (scrollbarNode->Y == previousScrollbarY) return;
+                    if (scrollbarNode->Y == previousScrollbarY)
+                        return;
 
                     previousScrollbarY = scrollbarNode->Y;
                 }
 
-                //var nqMarked = false;
-                //var hqMarked = false;
+                var listings = new List<ListItem>();
+
                 var firstListItemIndex = 0;
-
-                AtkComponentListItemRenderer* nqItemToMark = null;
-                AtkComponentListItemRenderer* hqItemToMark = null;
-
                 for (var i = 0; i < listingList->ListLength; i++)
                 {
                     var listItem = listingList->ItemRendererList[i];
                     var listItemIndex = listItem.AtkComponentListItemRenderer->ListItemIndex;
 
-                    if (i == 0) firstListItemIndex = listItemIndex;
+                    if (i == 0)
+                        firstListItemIndex = listItemIndex;
+                    // display list has "looped back" to the beginning (too many items to display), break out
                     else if (listItemIndex == firstListItemIndex)
-                    {
-                        // display list has "looped back" to the beginning (too many items to display), break out
                         break;
-                    }
 
-                    var itemQualityImageNode = (AtkTextNode*)listItem.AtkComponentListItemRenderer->GetImageNodeById(AddonItemQualityImageNodeId);
-                    if (itemQualityImageNode == null) continue;
+                    listings.Add(listItem);
+                }
+
+                // sort such that first list item at beginning
+                listings = listings
+                    .OrderBy(item => item.AtkComponentListItemRenderer->ListItemIndex)
+                    .ToList();
+
+                var nqNeededRem = nqNeeded;
+                var hqNeededRem = hqNeeded;
+                foreach (var listItem in listings)
+                {
+                    var itemQualityImageNode = listItem.AtkComponentListItemRenderer->GetImageNodeById(AddonItemQualityImageNodeId);
+                    if (itemQualityImageNode == null)
+                        continue;
 
                     var itemIsHq = itemQualityImageNode->IsVisible();
 
+                    var listingNode = (AtkResNode*)listItem.AtkComponentListItemRenderer->OwnerNode;
 
-                    if (nqNeeded && !itemIsHq && (nqItemToMark == null || listItemIndex < nqItemToMark->ListItemIndex)) // needed hq
+                    if (nqNeededRem > 0 && !itemIsHq) // needed nq
                     {
-                        // unmark old candidate
-                        if (nqItemToMark != null)
-                        {
-                            setNodeNeededMark((AtkResNode*)nqItemToMark->OwnerNode, false, true, true);
-                        }
-                        // set as new candidate
-                        nqItemToMark = listItem.AtkComponentListItemRenderer;
+                        // mark the node
+                        setNodeNeededMark(listingNode, true, true, true);
 
+                        // subtract the node's listing quantity from the remaining needed quantity
+                        var listingQuantityNode = (AtkTextNode*)listItem.AtkComponentListItemRenderer->GetTextNodeById(AddonItemQuantityNodeId);
+                        var listingQuantity = int.TryParse(listingQuantityNode->GetText().AsDalamudSeString().TextValue, out var parseResult)
+                            ? parseResult
+                            : 0;
+
+                        nqNeededRem -= listingQuantity;
                     }
-                    else if (hqNeeded && itemIsHq && (hqItemToMark == null || listItemIndex < hqItemToMark->ListItemIndex)) // needed nq
+                    else if (hqNeededRem > 0 && itemIsHq) // needed hq
                     {
-                        // unmark old candidate
-                        if (hqItemToMark != null)
-                        {
-                            setNodeNeededMark((AtkResNode*)hqItemToMark->OwnerNode, false, true, true);
-                        }
-                        // set as new candidate
-                        hqItemToMark = listItem.AtkComponentListItemRenderer;
+                        // mark the node
+                        setNodeNeededMark(listingNode, true, true, true);
 
+                        // subtract the node's listing quantity from the remaining needed quantity
+                        var listingQuantityNode = (AtkTextNode*)listingNode->GetComponent()->GetTextNodeById(AddonItemQuantityNodeId);
+                        var listingQuantity = int.TryParse(listingQuantityNode->GetText().AsDalamudSeString().TextValue, out var parseResult)
+                            ? parseResult
+                            : 0;
+                        hqNeededRem -= listingQuantity;
                     }
                     else // unneeded
-                    {
-                        setNodeNeededMark((AtkResNode*)listItem.AtkComponentListItemRenderer->OwnerNode, false, true, true);
-                    }
-                }
-
-                // mark final candidate items
-                if (nqItemToMark != null)
-                {
-                    setNodeNeededMark((AtkResNode*)nqItemToMark->OwnerNode, true, true, true);
-                }
-                if (hqItemToMark != null)
-                {
-                    setNodeNeededMark((AtkResNode*)hqItemToMark->OwnerNode, true, true, true);
+                        setNodeNeededMark(listingNode, false, true, true);
                 }
             }
             catch (Exception ex)
@@ -176,8 +188,8 @@ namespace BisBuddy.EventListeners.AddonEventListeners
         {
             try
             {
-                nqNeeded = false;
-                hqNeeded = false;
+                nqNeeded = 0;
+                hqNeeded = 0;
                 var infoProxy = InfoProxyItemSearch.Instance();
 
                 if (infoProxy == null) return;
@@ -185,8 +197,10 @@ namespace BisBuddy.EventListeners.AddonEventListeners
                 var nqItemId = infoProxy->SearchItemId;
                 var hqItemId = Plugin.ItemData.ConvertItemIdToHq(nqItemId);
 
-                nqNeeded = Gearset.GetGearsetsNeedingItemById(nqItemId, Plugin.Gearsets).Count > 0;
-                hqNeeded = hqItemId != nqItemId && Gearset.GetGearsetsNeedingItemById(hqItemId, Plugin.Gearsets).Count > 0;
+                nqNeeded = Gearset.GetGearsetsNeedingItemById(nqItemId, Plugin.Gearsets).Sum(gearset => gearset.countNeeded);
+                hqNeeded = hqItemId != nqItemId
+                    ? Gearset.GetGearsetsNeedingItemById(hqItemId, Plugin.Gearsets).Sum(gearset => gearset.countNeeded)
+                    : 0;
 
                 // ensure this update refreshes the draws by resetting caches
                 previousScrollbarY = -1.0f;
