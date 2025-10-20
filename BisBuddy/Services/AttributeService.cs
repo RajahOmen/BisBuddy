f2380afb2e1edbd41a249d1623771a8543b9c68c
RajahOmen <noah@reinrag.net>
2025-10-19 20:55:40 -0400

    Attempt to fix cache issues

--- BisBuddy/Services/AttributeService.cs
+++ BisBuddy/Services/AttributeService.cs
@@ -10,17 +10,29 @@ namespace BisBuddy.Services
     {
         private static readonly MemoryCacheEntryOptions CacheOptions = new()
         {
-            SlidingExpiration = TimeSpan.FromSeconds(5),
+            SlidingExpiration = TimeSpan.FromSeconds(1),
         };
 
         private readonly IMemoryCache memoryCache = memoryCache;
 
-        public T? GetEnumAttribute<T>(Enum enumValue) where T : Attribute =>
-            memoryCache.GetOrCreate(
-                (enumValue, typeof(T)),
-                cacheEntry => enumValue.GetAttribute<T>(),
-                CacheOptions
-                );
+        public T? GetEnumAttribute<T>(Enum enumValue) where T : Attribute
+        {
+            if (enumValue is null)
+                throw new ArgumentException("enumValue cannot be null");
+
+            var cacheKey = (enumValue, typeof(T));
+
+            if (memoryCache.TryGetValue(cacheKey, out var cacheValue))
+                return (T?) cacheValue;
+
+            var newCacheValue = EnumExtensions.GetAttribute<T>(enumValue)
+                ?? throw new ArgumentException($"Enum value {Enum.GetName(enumValue.GetType(), enumValue)} has no {typeof(T).Name}");
+
+            memoryCache.Set(cacheKey, newCacheValue, CacheOptions);
+
+            return newCacheValue;
+        }
+
     }
 
     public interface IAttributeService
